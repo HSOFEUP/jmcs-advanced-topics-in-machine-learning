ba277baf5e3070c159402f1078111dae5d52a065
Lukas Zbinden <>
2018-03-23 22:39:14 +0100

    start tests

---
+++ Assignment_01/README.txt
@@ -0,0 +1,13 @@
+H = 128
+dhcp-3-42:Assignment_01 lukaszbinden$ python assign01.py 
+total_loss_epoch ( 0 ) =  0.0 	diff_prev =  0
+total_loss_epoch ( 1 ) =  3752615.9552457277 	diff_prev =  375261595.5245728
+total_loss_epoch ( 2 ) =  52921.26474202596 	diff_prev =  -369969469.0503702
+total_loss_epoch ( 3 ) =  52738.542530579616 	diff_prev =  -18272.2211446343
+total_loss_epoch ( 4 ) =  52699.68183831405 	diff_prev =  -3886.0692265564576
+total_loss_epoch ( 5 ) =  52689.9505234485 	diff_prev =  -973.1314865555614
+total_loss_epoch ( 6 ) =  52687.250731097374 	diff_prev =  -269.97923511173576
+total_loss_epoch ( 7 ) =  52686.42977819769 	diff_prev =  -82.09528996795416
+total_loss_epoch ( 8 ) =  52686.15730039177 	diff_prev =  -27.24778059218079
+total_loss_epoch ( 9 ) =  52686.06097063858 	diff_prev =  -9.63297531940043
+===> Avg. PSNR: 9.4062 dB
--- Assignment_01/assign01.py
+++ Assignment_01/assign01.py
@@ -10,7 +10,7 @@ input_size = 784
 hidden_size = 128
 num_epochs = 10
 batch_size = 100
-learning_rate = 1e-4  # 0.001  # adjust: e.g 1e-4
+learning_rate = 1e-5  # 0.001  # adjust: e.g 1e-4
 
 # MNIST Dataset 
 train_dataset = dsets.MNIST(root='./data', 
@@ -36,8 +36,8 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 N = batch_size
 M = input_size
 H = hidden_size
-W1 = torch.rand(M, H)
-W2 = torch.rand(H, M)
+W1 = torch.randn(M, H)
+W2 = torch.randn(H, M)
 
 
 def non(X):
@@ -48,69 +48,71 @@ def non_prime(X):
     return torch.clamp(X, -1, 0) + 1
 
 
-def calc_grad_W1(X, W1, W2):
-    XW1 = torch.mm(X, W1)
-    nonXW1 = non(XW1)
-    nonXW1W2 = torch.mm(nonXW1, W2)
-    derivX_hat = 2 * (nonXW1W2 - X)
-
-    transpX = torch.transpose(X, 0, 1)
-    transpW2 = torch.transpose(W2, 0, 1)
-    nonprimeXW1 = non_prime(XW1)
-
-
-
-    assert(XW1.size()[0] == N)
-    assert(XW1.size()[1] == H)
-    assert(nonXW1.size()[0] == N)
-    assert(nonXW1.size()[1] == H)
-    assert(nonXW1W2.size()[0] == N)
-    assert(nonXW1W2.size()[1] == M)
-    assert(derivX_hat.size()[0] == N)
-    assert(derivX_hat.size()[1] == M)
-    assert(transpX.size()[0] == M)
-    assert(transpX.size()[1] == N)
-    assert(transpW2.size()[0] == M)
-    assert(transpW2.size()[1] == H)
-    assert(nonprimeXW1.size()[0] == N)
-    assert(nonprimeXW1.size()[1] == H)
-
-    result = torch.mm(transpX, torch.mm(derivX_hat, transpW2) * nonprimeXW1)
-    # assert dimension equal to W1
-    assert(result.size()[0] == M)
-    assert(result.size()[1] == H)
-    return result
+# def calc_grad_W1(X, W1, W2):
+#     XW1 = torch.mm(X, W1)
+#     nonXW1 = non(XW1)
+#     nonXW1W2 = torch.mm(nonXW1, W2)
+#     derivX_hat = 2 * (nonXW1W2 - X)
+#
+#     transpX = torch.transpose(X, 0, 1)
+#     transpW2 = torch.transpose(W2, 0, 1)
+#     nonprimeXW1 = non_prime(XW1)
+#
+#     assert(XW1.size()[0] == N)
+#     assert(XW1.size()[1] == H)
+#     assert(nonXW1.size()[0] == N)
+#     assert(nonXW1.size()[1] == H)
+#     assert(nonXW1W2.size()[0] == N)
+#     assert(nonXW1W2.size()[1] == M)
+#     assert(derivX_hat.size()[0] == N)
+#     assert(derivX_hat.size()[1] == M)
+#     assert(transpX.size()[0] == M)
+#     assert(transpX.size()[1] == N)
+#     assert(transpW2.size()[0] == M)
+#     assert(transpW2.size()[1] == H)
+#     assert(nonprimeXW1.size()[0] == N)
+#     assert(nonprimeXW1.size()[1] == H)
+#
+#     grad_W1 = torch.mm(transpX, torch.mm(derivX_hat, transpW2) * nonprimeXW1)
+#     # assert dimension equal to W1
+#     assert(grad_W1.size()[0] == M)
+#     assert(grad_W1.size()[1] == H)
+#     return grad_W1
 
 
-def calc_grad_W2(X, W1, W2):
-    XW1 = torch.mm(X, W1)
-    nonXW1 = non(XW1)
-    nonXW1W2 = torch.mm(nonXW1, W2)
-    derivX_hat = 2 * (nonXW1W2 - X)
+# def calc_grad_W2(X, W1, W2):
+#     XW1 = torch.mm(X, W1)
+#     nonXW1 = non(XW1)
+#     nonXW1W2 = torch.mm(nonXW1, W2)
+#     derivX_hat = 2 * (nonXW1W2 - X)
+#
+#     transp_nonXW1 = torch.transpose(nonXW1, 0, 1)
+#
+#     assert(XW1.size()[0] == N)
+#     assert(XW1.size()[1] == H)
+#     assert(nonXW1.size()[0] == N)
+#     assert(nonXW1.size()[1] == H)
+#     assert(nonXW1W2.size()[0] == N)
+#     assert(nonXW1W2.size()[1] == M)
+#     assert(derivX_hat.size()[0] == N)
+#     assert(derivX_hat.size()[1] == M)
+#     assert(transp_nonXW1.size()[0] == H)
+#     assert(transp_nonXW1.size()[1] == N)
+#
+#     grad_W2 = torch.mm(transp_nonXW1, derivX_hat)
+#     # assert dimension equal to W2
+#     assert(grad_W2.size()[0] == H)
+#     assert(grad_W2.size()[1] == M)
+#     return grad_W2
 
-    transp_nonXW1 = torch.transpose(nonXW1, 0, 1)
-
-    assert(XW1.size()[0] == N)
-    assert(XW1.size()[1] == H)
-    assert(nonXW1.size()[0] == N)
-    assert(nonXW1.size()[1] == H)
-    assert(nonXW1W2.size()[0] == N)
-    assert(nonXW1W2.size()[1] == M)
-    assert(derivX_hat.size()[0] == N)
-    assert(derivX_hat.size()[1] == M)
-    assert(transp_nonXW1.size()[0] == H)
-    assert(transp_nonXW1.size()[1] == N)
-
-    result = torch.mm(transp_nonXW1, derivX_hat)
-    # assert dimension equal to W2
-    assert(result.size()[0] == H)
-    assert(result.size()[1] == M)
-    return result
-
-
+total_loss_epoch = 0
+total_loss_epoch_prev = 0
 
 # Train the Model
 for epoch in range(num_epochs):
+    print('total_loss_epoch (', epoch, ') = ', (total_loss_epoch/batch_size), '\tdiff_prev = ', (total_loss_epoch - total_loss_epoch_prev))
+    total_loss_epoch_prev = total_loss_epoch
+    total_loss_epoch = 0
     for i, (images, _, ) in enumerate(train_loader):
         # Convert torch tensor to Variable
         images = images.view(-1, 28*28)
@@ -118,19 +120,76 @@ for epoch in range(num_epochs):
         X = targets  # for convenience with formulas
         
         # forward pass
-        X_hat = torch.mm(non(torch.mm(X, W1)), W2)
+        # X_hat = torch.mm(non(torch.mm(X, W1)), W2)
+        XW1 = torch.mm(X, W1)
+        nonXW1 = non(XW1)
+        nonXW1W2 = torch.mm(nonXW1, W2)
+        X_hat = nonXW1W2
+        assert(X_hat.size()[0] == X.size()[0])
+        assert(X_hat.size()[1] == X.size()[1])
 
         # loss calculation
-        loss = torch.sum((X_hat - targets)**2)
+        loss = torch.sum((X_hat - X)**2)
+        total_loss_epoch = total_loss_epoch + loss
 
         # gradient calculation and update parameters
-        W1 = W1 - learning_rate * calc_grad_W1(X, W1, W2)
-        W2 = W2 - learning_rate * calc_grad_W2(X, W1, W2)
+        # grad_W1 --------------------------------------------------
+        derivX_hat = 2 * (nonXW1W2 - X)
+        transpX = torch.transpose(X, 0, 1)
+        transpW2 = torch.transpose(W2, 0, 1)
+        nonprimeXW1 = non_prime(XW1)
+
+        assert(XW1.size()[0] == N)
+        assert(XW1.size()[1] == H)
+        assert(nonXW1.size()[0] == N)
+        assert(nonXW1.size()[1] == H)
+        assert(nonXW1W2.size()[0] == N)
+        assert(nonXW1W2.size()[1] == M)
+        assert(derivX_hat.size()[0] == N)
+        assert(derivX_hat.size()[1] == M)
+        assert(transpX.size()[0] == M)
+        assert(transpX.size()[1] == N)
+        assert(transpW2.size()[0] == M)
+        assert(transpW2.size()[1] == H)
+        assert(nonprimeXW1.size()[0] == N)
+        assert(nonprimeXW1.size()[1] == H)
+
+        grad_W1 = torch.mm(transpX, torch.mm(derivX_hat, transpW2) * nonprimeXW1)
+        # assert dimension equal to W1
+        assert(grad_W1.size()[0] == M)
+        assert(grad_W1.size()[1] == H)
+        assert(grad_W1.size()[0] == W1.size()[0])
+        assert(grad_W1.size()[1] == W1.size()[1])
+
+        # print('grad_W1 = ', torch.norm(grad_W1))
+        diff = learning_rate * grad_W1
+        # print('diff_W1 = ', torch.norm(diff))
+        W1 = W1 - diff
+
+        # grad_W2 --------------------------------------------------
+        transp_nonXW1 = torch.transpose(nonXW1, 0, 1)
+
+        assert(transp_nonXW1.size()[0] == H)
+        assert(transp_nonXW1.size()[1] == N)
+
+        grad_W2 = torch.mm(transp_nonXW1, derivX_hat)
+        # assert dimension equal to W2
+        assert(grad_W2.size()[0] == H)
+        assert(grad_W2.size()[1] == M)
+        assert(grad_W2.size()[0] == W2.size()[0])
+        assert(grad_W2.size()[1] == W2.size()[1])
+
+        # print('grad_W2 = ', torch.norm(grad_W2))
+        diff = learning_rate * grad_W2
+        # print('diff_W2 = ', torch.norm(diff))
+        W2 = W2 - diff
 
         # check your loss 
-        if (i+1) % 1 == 0:
-            print ('Epoch [%d/%d], Step [%d/%d], Loss: %.4f' 
-                   %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss))
+        #if (i+1) % 200 == 0:
+            # print('norm W1: ', torch.norm(W1))
+            # print('norm W2: ', torch.norm(W2))
+            #print ('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
+            #       %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss))
 
 # Test the Model
 avg_psnr = 0
