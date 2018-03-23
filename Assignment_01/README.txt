e1c12549ae9d425ef357fd8e0e55aa5e8f6a7610
Lukas Zbinden <>
2018-03-23 23:04:55 +0100

    cleanup and log for different H

--- Assignment_01/README.txt
+++ Assignment_01/README.txt
@@ -11,3 +11,78 @@ total_loss_epoch ( 7 ) =  52686.42977819769 	diff_prev =  -82.09528996795416
 total_loss_epoch ( 8 ) =  52686.15730039177 	diff_prev =  -27.24778059218079
 total_loss_epoch ( 9 ) =  52686.06097063858 	diff_prev =  -9.63297531940043
 ===> Avg. PSNR: 9.4062 dB
+
+H = 256
+
+dhcp-3-42:Assignment_01 lukaszbinden$ python assign01.py 
+total_loss_epoch ( 0 ) =  0.0 	diff_prev =  0
+total_loss_epoch ( 1 ) =  29725260.108946566 	diff_prev =  2972526010.8946567
+total_loss_epoch ( 2 ) =  52847.68264001526 	diff_prev =  -2967241242.6306553
+total_loss_epoch ( 3 ) =  52717.824399335266 	diff_prev =  -12985.824067999609
+total_loss_epoch ( 4 ) =  52694.4792220633 	diff_prev =  -2334.5177271962166
+total_loss_epoch ( 5 ) =  52688.776240432366 	diff_prev =  -570.2981630936265
+total_loss_epoch ( 6 ) =  52687.03040656026 	diff_prev =  -174.58338721003383
+total_loss_epoch ( 7 ) =  52686.41871337045 	diff_prev =  -61.16931898146868
+total_loss_epoch ( 8 ) =  52686.17992520884 	diff_prev =  -23.878816161304712
+total_loss_epoch ( 9 ) =  52686.08079444062 	diff_prev =  -9.91307682171464
+===> Avg. PSNR: 9.3126 dB
+
+H = 384
+
+dhcp-3-42:Assignment_01 lukaszbinden$ python assign01.py 
+total_loss_epoch ( 0 ) =  0.0 	diff_prev =  0
+total_loss_epoch ( 1 ) =  299362183.6453562 	diff_prev =  29936218364.535618
+total_loss_epoch ( 2 ) =  53044.93561685647 	diff_prev =  -29930913870.97393
+total_loss_epoch ( 3 ) =  52807.87807682091 	diff_prev =  -23705.754003555514
+total_loss_epoch ( 4 ) =  52735.68520492362 	diff_prev =  -7219.287189729512
+total_loss_epoch ( 5 ) =  52708.18622726848 	diff_prev =  -2749.8977655135095
+total_loss_epoch ( 6 ) =  52696.40881495106 	diff_prev =  -1177.741231742315
+total_loss_epoch ( 7 ) =  52691.04286577141 	diff_prev =  -536.5949179651216
+total_loss_epoch ( 8 ) =  52688.49986169907 	diff_prev =  -254.300407233648
+total_loss_epoch ( 9 ) =  52687.26718305451 	diff_prev =  -123.26786445640028
+===> Avg. PSNR: 9.3831 dB
+
+H = 64
+
+dhcp-3-42:Assignment_01 lukaszbinden$ python assign01.py 
+total_loss_epoch ( 0 ) =  0.0 	diff_prev =  0
+total_loss_epoch ( 1 ) =  2590433.143782349 	diff_prev =  259043314.3782349
+total_loss_epoch ( 2 ) =  52711.96979871877 	diff_prev =  -253772117.39836302
+total_loss_epoch ( 3 ) =  52691.02888552481 	diff_prev =  -2094.0913193961605
+total_loss_epoch ( 4 ) =  52687.266613098596 	diff_prev =  -376.22724262066185
+total_loss_epoch ( 5 ) =  52686.3627002317 	diff_prev =  -90.3912866897881
+total_loss_epoch ( 6 ) =  52686.12153772292 	diff_prev =  -24.116250878199935
+total_loss_epoch ( 7 ) =  52686.04588377159 	diff_prev =  -7.565395132638514
+total_loss_epoch ( 8 ) =  52686.020285832135 	diff_prev =  -2.559793945401907
+total_loss_epoch ( 9 ) =  52686.01109207735 	diff_prev =  -0.9193754782900214
+===> Avg. PSNR: 9.3826 dB
+
+H = 32
+
+dhcp-3-42:Assignment_01 lukaszbinden$ python assign01.py 
+total_loss_epoch ( 0 ) =  0.0 	diff_prev =  0
+total_loss_epoch ( 1 ) =  1060175.2755078934 	diff_prev =  106017527.55078934
+total_loss_epoch ( 2 ) =  52693.02104721325 	diff_prev =  -100748225.44606802
+total_loss_epoch ( 3 ) =  52686.30689632354 	diff_prev =  -671.4150889711455
+total_loss_epoch ( 4 ) =  52686.02893991381 	diff_prev =  -27.795640973374248
+total_loss_epoch ( 5 ) =  52686.01182231287 	diff_prev =  -1.7117600943893194
+total_loss_epoch ( 6 ) =  52686.010667366136 	diff_prev =  -0.115494673140347
+total_loss_epoch ( 7 ) =  52686.01048787127 	diff_prev =  -0.017949487082660198
+total_loss_epoch ( 8 ) =  52686.01038347407 	diff_prev =  -0.010439719073474407
+total_loss_epoch ( 9 ) =  52686.00959956111 	diff_prev =  -0.07839129585772753
+===> Avg. PSNR: 9.4778 dB
+
+H = 16
+
+dhcp-3-42:Assignment_01 lukaszbinden$ python assign01.py 
+total_loss_epoch ( 0 ) =  0.0 	diff_prev =  0
+total_loss_epoch ( 1 ) =  736279.9971948826 	diff_prev =  73627999.71948826
+total_loss_epoch ( 2 ) =  52686.013658691234 	diff_prev =  -68359398.35361914
+total_loss_epoch ( 3 ) =  52686.01365869124 	diff_prev =  9.313225746154785e-10
+total_loss_epoch ( 4 ) =  52686.01365869136 	diff_prev =  1.1175870895385742e-08
+total_loss_epoch ( 5 ) =  52686.01365869129 	diff_prev =  -6.51925802230835e-09
+total_loss_epoch ( 6 ) =  52686.01365869126 	diff_prev =  -2.7939677238464355e-09
+total_loss_epoch ( 7 ) =  52686.01365869132 	diff_prev =  5.587935447692871e-09
+total_loss_epoch ( 8 ) =  52686.013658691336 	diff_prev =  1.862645149230957e-09
+total_loss_epoch ( 9 ) =  52686.01365869136 	diff_prev =  1.862645149230957e-09
+===> Avg. PSNR: 9.4779 dB
--- Assignment_01/assign01.py
+++ Assignment_01/assign01.py
@@ -4,13 +4,14 @@ import numpy as np
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from math import log10
+import matplotlib.pyplot as plt
 
 # Hyper Parameters 
 input_size = 784
 hidden_size = 128
 num_epochs = 10
 batch_size = 100
-learning_rate = 1e-5  # 0.001  # adjust: e.g 1e-4
+learning_rate = 1e-5  # 0.001
 
 # MNIST Dataset 
 train_dataset = dsets.MNIST(root='./data', 
@@ -48,63 +49,6 @@ def non_prime(X):
     return torch.clamp(X, -1, 0) + 1
 
 
-# def calc_grad_W1(X, W1, W2):
-#     XW1 = torch.mm(X, W1)
-#     nonXW1 = non(XW1)
-#     nonXW1W2 = torch.mm(nonXW1, W2)
-#     derivX_hat = 2 * (nonXW1W2 - X)
-#
-#     transpX = torch.transpose(X, 0, 1)
-#     transpW2 = torch.transpose(W2, 0, 1)
-#     nonprimeXW1 = non_prime(XW1)
-#
-#     assert(XW1.size()[0] == N)
-#     assert(XW1.size()[1] == H)
-#     assert(nonXW1.size()[0] == N)
-#     assert(nonXW1.size()[1] == H)
-#     assert(nonXW1W2.size()[0] == N)
-#     assert(nonXW1W2.size()[1] == M)
-#     assert(derivX_hat.size()[0] == N)
-#     assert(derivX_hat.size()[1] == M)
-#     assert(transpX.size()[0] == M)
-#     assert(transpX.size()[1] == N)
-#     assert(transpW2.size()[0] == M)
-#     assert(transpW2.size()[1] == H)
-#     assert(nonprimeXW1.size()[0] == N)
-#     assert(nonprimeXW1.size()[1] == H)
-#
-#     grad_W1 = torch.mm(transpX, torch.mm(derivX_hat, transpW2) * nonprimeXW1)
-#     # assert dimension equal to W1
-#     assert(grad_W1.size()[0] == M)
-#     assert(grad_W1.size()[1] == H)
-#     return grad_W1
-
-
-# def calc_grad_W2(X, W1, W2):
-#     XW1 = torch.mm(X, W1)
-#     nonXW1 = non(XW1)
-#     nonXW1W2 = torch.mm(nonXW1, W2)
-#     derivX_hat = 2 * (nonXW1W2 - X)
-#
-#     transp_nonXW1 = torch.transpose(nonXW1, 0, 1)
-#
-#     assert(XW1.size()[0] == N)
-#     assert(XW1.size()[1] == H)
-#     assert(nonXW1.size()[0] == N)
-#     assert(nonXW1.size()[1] == H)
-#     assert(nonXW1W2.size()[0] == N)
-#     assert(nonXW1W2.size()[1] == M)
-#     assert(derivX_hat.size()[0] == N)
-#     assert(derivX_hat.size()[1] == M)
-#     assert(transp_nonXW1.size()[0] == H)
-#     assert(transp_nonXW1.size()[1] == N)
-#
-#     grad_W2 = torch.mm(transp_nonXW1, derivX_hat)
-#     # assert dimension equal to W2
-#     assert(grad_W2.size()[0] == H)
-#     assert(grad_W2.size()[1] == M)
-#     return grad_W2
-
 total_loss_epoch = 0
 total_loss_epoch_prev = 0
 
@@ -125,8 +69,6 @@ for epoch in range(num_epochs):
         nonXW1 = non(XW1)
         nonXW1W2 = torch.mm(nonXW1, W2)
         X_hat = nonXW1W2
-        assert(X_hat.size()[0] == X.size()[0])
-        assert(X_hat.size()[1] == X.size()[1])
 
         # loss calculation
         loss = torch.sum((X_hat - X)**2)
@@ -139,50 +81,14 @@ for epoch in range(num_epochs):
         transpW2 = torch.transpose(W2, 0, 1)
         nonprimeXW1 = non_prime(XW1)
 
-        assert(XW1.size()[0] == N)
-        assert(XW1.size()[1] == H)
-        assert(nonXW1.size()[0] == N)
-        assert(nonXW1.size()[1] == H)
-        assert(nonXW1W2.size()[0] == N)
-        assert(nonXW1W2.size()[1] == M)
-        assert(derivX_hat.size()[0] == N)
-        assert(derivX_hat.size()[1] == M)
-        assert(transpX.size()[0] == M)
-        assert(transpX.size()[1] == N)
-        assert(transpW2.size()[0] == M)
-        assert(transpW2.size()[1] == H)
-        assert(nonprimeXW1.size()[0] == N)
-        assert(nonprimeXW1.size()[1] == H)
-
         grad_W1 = torch.mm(transpX, torch.mm(derivX_hat, transpW2) * nonprimeXW1)
-        # assert dimension equal to W1
-        assert(grad_W1.size()[0] == M)
-        assert(grad_W1.size()[1] == H)
-        assert(grad_W1.size()[0] == W1.size()[0])
-        assert(grad_W1.size()[1] == W1.size()[1])
-
-        # print('grad_W1 = ', torch.norm(grad_W1))
-        diff = learning_rate * grad_W1
-        # print('diff_W1 = ', torch.norm(diff))
-        W1 = W1 - diff
+        W1 = W1 - learning_rate * grad_W1
 
         # grad_W2 --------------------------------------------------
         transp_nonXW1 = torch.transpose(nonXW1, 0, 1)
 
-        assert(transp_nonXW1.size()[0] == H)
-        assert(transp_nonXW1.size()[1] == N)
-
         grad_W2 = torch.mm(transp_nonXW1, derivX_hat)
-        # assert dimension equal to W2
-        assert(grad_W2.size()[0] == H)
-        assert(grad_W2.size()[1] == M)
-        assert(grad_W2.size()[0] == W2.size()[0])
-        assert(grad_W2.size()[1] == W2.size()[1])
-
-        # print('grad_W2 = ', torch.norm(grad_W2))
-        diff = learning_rate * grad_W2
-        # print('diff_W2 = ', torch.norm(diff))
-        W2 = W2 - diff
+        W2 = W2 - learning_rate * grad_W2
 
         # check your loss 
         #if (i+1) % 200 == 0:
@@ -204,3 +110,11 @@ for (images, _, ) in test_loader:
     psnr = 10 * log10(1 / mse)
     avg_psnr += psnr
 print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(test_loader)))
+
+# plot predictions
+# plt.plot(x_train, y_train, 'ro', label='Original data')
+# predicted = theta*x_train+b
+# plt.plot(x_train, predicted, label='Fitted line')
+# plt.legend()
+# plt.show()
+
