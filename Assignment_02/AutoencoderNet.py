6fae139a361f7c8a40d2b8a5ece2bfa3911ea5ab
Lukas Zbinden <>
2018-04-16 22:51:44 +0200

    define layers of autoencoder

--- Assignment_02/AutoencoderNet.py
+++ Assignment_02/AutoencoderNet.py
@@ -1,31 +1,39 @@
-
-
 import torch
 import torch.nn as nn
 
-class AutoencoderNet(nn.Module):
 
+class AutoencoderNet(nn.Module):
     def __init__(self):
         super(AutoencoderNet, self).__init__()
 
-        self.channel_num = 8
-        self.encoder = nn.Sequential( #TODO : encoder architecture
-            )
+        # Input size: 1 x 28 x 28 = 784 dimensions
 
-        self.decoder = nn.Sequential( #TODO : decoder architecture
-            )
+        # Calculation of output size: O = (W - K + 2P) / S + 1  ; O: output, W: input, K: kernel, P: padding, S: stride
+
+        self.channel_num = 8
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, stride=3, padding=2),  # 16 x 12 x 12
+            nn.MaxPool2d(kernel_size=3, stride=3),  # 16 x 4 x 4
+            nn.Conv2d(16, 8, kernel_size=2, stride=2)  # 8 x 2 x 2
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(8, 12, kernel_size=3, stride=2),  # 12 x 5 x 5
+            nn.ConvTranspose2d(12, 12, kernel_size=5, stride=3),  # 12 x 15 x 15
+            nn.ConvTranspose2d(12, 1, kernel_size=2, stride=2),  # 1 x 30 x 30
+        )
 
     def forward(self, x):
         en = self.encoder(x)
-        out  = self.decoder(en)
+        out = self.decoder(en)
 
-        #crop the centeral box with the same size as input.
-        x_off = int((out.shape[2]-x.shape[2])/2)
-        y_off = int((out.shape[3]-x.shape[3])/2)
-        out = out[:,:,x_off:x_off+x.shape[2], y_off:y_off+x.shape[3]]
+        # crop the centeral box with the same size as input.
+        x_off = int((out.shape[2] - x.shape[2]) / 2)
+        y_off = int((out.shape[3] - x.shape[3]) / 2)
+        out = out[:, :, x_off:x_off + x.shape[2], y_off:y_off + x.shape[3]]
         return out
 
     def get_features(self, x):
         en = self.encoder(x)
-        en  = en.view(-1, 32)
+        en = en.view(-1, 32)
         return en
--- Assignment_02/assignment02.py
+++ Assignment_02/assignment02.py
@@ -1,5 +1,6 @@
+# You are allowed to use all the modules and packages of pytorch, numpy
 
-
+import torch.nn as nn
 import torch
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
@@ -9,7 +10,7 @@ import imageio
 import os
 import numpy as np
 from AutoencoderNet import AutoencoderNet
-
+from torch.autograd import Variable
 from math import log10
 
 # Hyper Parameters
@@ -19,6 +20,7 @@ batch_size = 100
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
+
     def __init__(self):
         self.reset()
 
@@ -33,6 +35,7 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+
 
 def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
@@ -49,17 +52,17 @@ def accuracy(output, target, topk=(1,)):
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
 
-def TrainAutoencoder(train_loader, test_loader, num_epochs ):
 
-    #Trains autoencoder and returns the trained model.
-    #The trained model will be used as feature extractor to train linear classifier.
+def TrainAutoencoder(train_loader, test_loader, num_epochs):
+    # Trains autoencoder and returns the trained model.
+    # The trained model will be used as feature extractor to train linear classifier.
 
     lr = 1e-3
     model = AutoencoderNet()
 
-    optimizer = #TODO : define ans set the optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)  # TODO : define ans set the optimizer
 
-    criterion = #TODO : define the loss function
+    criterion = nn.CrossEntropyLoss()  # TODO : define the loss function
 
     for epoch in range(num_epochs):
 
@@ -68,53 +71,50 @@ def TrainAutoencoder(train_loader, test_loader, num_epochs ):
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr
 
-        #Training loop for an epoch
+        # Training loop for an epoch
         for i, (images, _) in enumerate(train_loader):
 
-            #TODO : implement a full step of optimization.
+            # TODO : implement a full step of optimization.
+            # Convert torch tensor to Variable
+            images = Variable(images)
+            labels = images
 
+            # Forward + Backward + Optimize
+            optimizer.zero_grad()  # zero the gradient buffer
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-
-
-
-            if (i+1) % 100 == 0:
-                print ('Autoencoder Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
-                       %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss))
-
-
-
+            if (i + 1) % 100 == 0:
+                print('Autoencoder Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
+                      % (epoch + 1, num_epochs, i + 1, len(train_dataset) // batch_size, loss))
 
         model.eval()
-        pred_imgs = np.zeros((100*28,200*28), dtype=np.uint8) + 100
+        pred_imgs = np.zeros((100 * 28, 200 * 28), dtype=np.uint8) + 100
         avg_psnr = 0
         # Testing loop for an epoch
         for images, labels in test_loader:
-
-
-
-            #TODO : calculate the outputs
+            # TODO : calculate the outputs
 
             mse = torch.mean((outputs - input_var.cpu()).pow(2))
             psnr = 10 * log10(1 / mse)
             avg_psnr += psnr
 
-
         print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(test_loader)))
 
     return model
 
-
-
-def TrainLinearClassifier(model, train_loader, test_loader, num_epochs ):
-
+'''
+def TrainLinearClassifier(model, train_loader, test_loader, num_epochs):
     lr = 1e-3
     model.eval()
-    #Get the feature dimensin by passing a random input
-    feat_dim = model.get_features(torch.autograd.Variable(torch.FloatTensor(1,1,28,28))).shape[1]
+    # Get the feature dimension by passing a random input
+    feat_dim = model.get_features(torch.autograd.Variable(torch.FloatTensor(1, 1, 28, 28))).shape[1]
 
-    lin_criterion =  #TODO : define the loss
-    lin_model = torch.nn.Sequential( torch.nn.Linear(feat_dim, 10) )
-    lin_optimizer = #TODO : define the optimizer
+    lin_criterion =  # TODO : define the loss
+    lin_model = torch.nn.Sequential(torch.nn.Linear(feat_dim, 10))
+    lin_optimizer =  # TODO : define the optimizer
 
     top1 = AverageMeter()
     top5 = AverageMeter()
@@ -128,21 +128,21 @@ def TrainLinearClassifier(model, train_loader, test_loader, num_epochs ):
         for param_group in lin_optimizer.param_groups:
             param_group['lr'] = lr
 
-        #training for loop
+        # training for loop
         for i, (images, target) in enumerate(train_loader):
 
-            #target includes the character labels
-            #get the encoder features
+            # target includes the character labels
+            # get the encoder features
             feats = model.get_features(torch.autograd.Variable(images))
 
-            #TODO : implement a full step of optimization.
+            # TODO : implement a full step of optimization.
 
             prec1, prec5 = accuracy(pred.data, target, topk=(1, 5))
             top1.update(prec1[0], images.size(0))
             top5.update(prec5[0], images.size(0))
 
             if i % 100 == 0:
-                print('Linear classifier Epoch: [{0}][{1}/{2}]\t'                      
+                print('Linear classifier Epoch: [{0}][{1}/{2}]\t'
                       'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
                       'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
                     epoch, i, len(train_loader), top1=top1, top5=top5))
@@ -151,20 +151,16 @@ def TrainLinearClassifier(model, train_loader, test_loader, num_epochs ):
         top1.reset()
         top5.reset()
         for i, (images, target) in enumerate(test_loader):
+            # TODO : extract the feature and compute classifier predictions
 
-                #TODO : extract the feature and compute classifier predictions
-
-                prec1, prec5 = accuracy(pred.data, target, topk=(1, 5))
-                top1.update(prec1[0], images.size(0))
-                top5.update(prec5[0], images.size(0))
+            prec1, prec5 = accuracy(pred.data, target, topk=(1, 5))
+            top1.update(prec1[0], images.size(0))
+            top5.update(prec5[0], images.size(0))
 
         print('===>>>\t'
               'Prec@1 ({top1.avg:.3f})\t'
               'Prec@5 ({top5.avg:.3f})'.format(top1=top1, top5=top5))
-
-
-
-
+'''
 
 # MNIST Dataset
 train_dataset = dsets.MNIST(root='./data',
@@ -185,6 +181,5 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=False)
 
-
-model = TrainAutoencoder(train_loader, test_loader, num_epochs )
-TrainLinearClassifier(model, train_loader, test_loader, num_epochs )
+model = TrainAutoencoder(train_loader, test_loader, num_epochs)
+#TrainLinearClassifier(model, train_loader, test_loader, num_epochs)
